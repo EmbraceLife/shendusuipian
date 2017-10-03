5292974840a7e36ab9b1e02b50939e88ae72de65
EmbraceLife <1227561934@qq.com>
2017-10-03 15:41:47 +0800

    organize

--- pytorch/ready/04cifar10_tutorial.py
+++ pytorch/ready/04cifar10_tutorial.py
@@ -67,12 +67,12 @@ transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+trainset = torchvision.datasets.CIFAR10(root='/Users/Natsume/Desktop/data', train=True,
                                         download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
                                           shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+testset = torchvision.datasets.CIFAR10(root='/Users/Natsume/Desktop/data', train=False,
                                        download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=4,
                                          shuffle=False, num_workers=2)
